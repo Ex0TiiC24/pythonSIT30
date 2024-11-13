1812e286f78d862d512545e288f919134eeff2f8
Ex0TiiC24 <taiyophamadadar6@gmail.com>
2024-11-13 09:30:49 +0700

    lol

--- code/c13_wed17.py
+++ code/c13_wed17.py
@@ -1,12 +1,18 @@
+from typing import Callable  # Import Callable from the typing module
 
-
-
-def double(i:int)-> int:
-    return i*2
+def double(i: int) -> int:
+    return i * 2
 
 print(double(23))
 
+# Use Callable with a capital 'C' and correct typing
+f: Callable[[int], int] = double
+print(f(35))
 
-f:callable[[int],int] = double
 
-print(f(22))
+def high(fc:Callable[[int],int],v:int):
+    return fc(v)
+
+
+print(high(f,12))
+print(high(double,31))

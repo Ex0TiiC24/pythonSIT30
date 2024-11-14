131cbefd798c74808d1052a42df48aebc8788262
Ex0TiiC24 <taiyophamadadar6@gmail.com>
2024-11-14 15:52:11 +0700

    update

--- code/c13_wed17.py
+++ code/c13_wed17.py
@@ -1,18 +1,43 @@
-from typing import Callable  # Import Callable from the typing module
+class Employee:
+    def __init__(self,eid,name,salary) -> None:
+        self._eid = eid
+        self._name = name
+        self._salary = salary
+    def get_eid(self):
+        return self._eid
+    def get_name(self):
+        return self._name
+    def get_salary(self):
+        return self._salary
+    def set_eid(self,new_eid):
+        self._eid = new_eid
+    def set_name(self,new_name):
+        self._name = new_name
+    def set_salary(self,new_salary):
+        self._salary = new_salary
+    def __repe__(self):
+        return f"Employee(eid={self._eid}, name={self._name}, salary={self._salary})"
 
-def double(i: int) -> int:
-    return i * 2
 
-print(double(23))
+siam = Employee(1,"Siam",670000000)
+olarn = Employee(2,"Olarn",20)
+lol = Employee(3,"kok",1000)
+banana = Employee(4,"Kluay",200)
+pop = Employee(5,"opoi",1600)
+peep = Employee(6,"peep",12345)
 
-# Use Callable with a capital 'C' and correct typing
-f: Callable[[int], int] = double
-print(f(35))
-
-
-def high(fc:Callable[[int],int],v:int):
-    return fc(v)
+all = [siam,olarn,lol,banana,pop,peep]
 
 
-print(high(f,12))
-print(high(double,31))
+def salary(employee):
+    return employee.get_salary() > 1000
+
+def name(employee):
+    return employee.get_name()
+
+filtered_employees = filter(salary, all)
+
+print(list(filtered_employees))
+
+mapped_employees = map(name,all)
+print(list(mapped_employees))
